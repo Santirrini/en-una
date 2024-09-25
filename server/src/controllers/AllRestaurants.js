3f62db5648de9778f2a39a7619146916432730b9
Ariel-Alegre <arielalegre98@gmail.com>
2024-09-25 09:07:41 -0300

    update server

--- server/src/controllers/AllRestaurants.js
+++ server/src/controllers/AllRestaurants.js
@@ -1,22 +1,28 @@
-const { Restaurant, User, Menu } = require('../db');
+// src/controllers/restaurantController.js
+const { Restaurant, Menu } = require('../db');
+const { wss } = require('../app'); // Importa el servidor WebSocket
+
 module.exports = {
   AllRestaurant: async (req, res) => {
     try {
-  
-        const restaurants = await Restaurant.findAll({
-          include: [
-            { model: Menu }
-          ]
-        });
+      const restaurants = await Restaurant.findAll({
+        include: [{ model: Menu }],
+      });
 
-        console.log('Todos los restaurantes');
+      console.log('Todos los restaurantes obtenidos');
 
-        res.status(200).send({success: true, data: restaurants})
+      // Responder a la solicitud HTTP
+      res.status(200).send({ success: true, data: restaurants });
 
-
+      // Enviar los datos a través de WebSocket a todos los clientes conectados
+      wss.clients.forEach((client) => {
+        if (client.readyState === client.OPEN) {
+          client.send(JSON.stringify({ success: true, data: restaurants }));
+        }
+      });
     } catch (error) {
-      console.error('error al obtener Todas las ordenes:', error);
-      res.status(500).json({ error: 'error al obtener Todas las ordenes' });
+      console.error('Error al obtener todos los restaurantes:', error);
+      res.status(500).json({ error: 'Error al obtener todos los restaurantes' });
     }
-  }
+  },
 };
