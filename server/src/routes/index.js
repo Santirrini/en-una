608284b41a4f4641d53553fa828f4f2069732a8c
Ariel-Alegre <arielalegre98@gmail.com>
2024-11-21 10:31:02 -0300

    update

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -2,7 +2,7 @@
 const { Router  }= require('express');
 const router = Router();
 const postRestaurantRouter = require('./restaurant_router');
-const postUserRouter = require('./users_router'); 
+const postUserRouter = require('../routes/users_router'); 
 const postMenuRouter = require('./menu_router'); 
 const passwordRouter = require('./password'); 
 
