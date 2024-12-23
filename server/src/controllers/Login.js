7a9f3689b0f11f5cc6609cfe6361542ddb760adb
Ariel-Alegre <arielalegre98@gmail.com>
2024-12-23 14:02:34 -0300

    update

--- server/src/controllers/Login.js
+++ server/src/controllers/Login.js
@@ -10,18 +10,18 @@ module.exports = {
     try {
       const user = await User.findOne({
         where: { email },
-        attributes: ['id', 'name', 'lastName','email', 'password', 'phone','status', 'role'],
+        attributes: ['id', 'name', 'lastName', 'email', 'password', 'phone', 'status', 'role'],
       });
 
       if (!user) {
         console.log('Usuario no encontrado');
         return res.status(404).json({ message: 'Usuario no encontrado' });
       }
-   // Verificar estado
-   if (user.status === 'pendiente') {
-    console.log('El usuario está en pendiente');
-    return res.status(403).send({ success: false, message: 'El usuario está en pendiente' });
-  }
+      // Verificar estado
+      if (user.status === 'pendiente') {
+        console.log('El usuario está en pendiente');
+        return res.status(403).send({ success: false, message: 'El usuario está en pendiente' });
+      }
 
       const isMatch = await bcrypt.compare(password, user.password);
 
@@ -39,12 +39,9 @@ module.exports = {
         phone: user.phone,
         role: user.role,
       };
-
-      const token = jwt.sign(tokenPayload, process.env.FIRMA_TOKEN);
-
+     const token = jwt.sign(tokenPayload, process.env.FIRMA_TOKEN);
       console.log('Inicio de sesión exitoso');
       return res.json({ token, role: user.role, userId: user.id, status: user.status });
-      
     } catch (error) {
       console.error(error);
       res.status(500).json({ message: 'Error en el servidor' });
