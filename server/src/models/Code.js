ffe5e81c8a2e12a7eea9a48d9bdcde3fa1f2be02
Ariel-Alegre <arielalegre98@gmail.com>
2024-12-26 13:57:01 -0300

    update

---
+++ server/src/models/Code.js
@@ -0,0 +1,17 @@
+const { DataTypes } = require('sequelize');
+
+module.exports = (sequelize) => {
+    const Code = sequelize.define('Code', {
+        id: {
+            type: DataTypes.STRING,
+            primaryKey: true,
+        },
+        code: {
+            type: DataTypes.STRING,
+            unique: true,
+          },
+        }, {
+          timestamps: false, // Desactiva los campos createdAt y updatedAt
+        });
+    return Code
+};
