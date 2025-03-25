835c986ae26d2c52c3259286fad985226a3cbd1d
Ariel Alegre <arielalegre98@gmail.com>
2025-03-25 14:55:40 -0300

    update

--- client/src/components/DetailsRestaurant/DetailsRestaurant.jsx
+++ client/src/components/DetailsRestaurant/DetailsRestaurant.jsx
@@ -54,7 +54,7 @@ L.Icon.Default.mergeOptions({
   shadowUrl: require("leaflet/dist/images/marker-shadow.png"),
 });
 const containerStyle = {
-  maxWidth: "400px",
+  maxWidth: "345px",
   height: "345px",
 };
 
